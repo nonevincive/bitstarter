8861c3b622bf121141d08042bee2fe91921d1c9d
Ubuntu <ubuntu@ip-172-31-43-128.us-west-2.compute.internal>
2013-07-06 14:47:03 +0000

    And again.

--- web.js
+++ web.js
@@ -2,13 +2,9 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-/*
 app.get('/', function(request, response) {
   response.send('Hello World 2!');
 });
-*/
-
-app.register('.html', require('jade'));
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
